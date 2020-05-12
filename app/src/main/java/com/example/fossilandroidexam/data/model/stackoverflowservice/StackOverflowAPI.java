e3651f9a2a1c4fd74549aaafada2b2ed83413156
Linh <linhmiyano99>
2020-05-12 13:31:03 +0700

    update: camelcase for StackOverflowAPI

--- app/src/main/java/com/example/fossilandroidexam/data/model/stackoverflowservice/StackOverflowAPI.java
+++ app/src/main/java/com/example/fossilandroidexam/data/model/stackoverflowservice/StackOverflowAPI.java
@@ -8,13 +8,13 @@ import retrofit2.http.Query;
 public interface StackOverflowAPI {
     String BASE_URL = "https://api.stackexchange.com";
 
-    @GET("/2.2/users?pagesize=30&site=stackoverflow")
+    @GET("/2.2/users?pageSize=30&site=stackOverflow")
     Call<ListWrapper<User>> getAllUsers(@Query("page") int numberOfPage);
 
-    @GET("/2.2/users/{ids}?order=desc&sort=reputation&site=stackoverflow")
+    @GET("/2.2/users/{ids}?order=desc&sort=reputation&site=stackOverflow")
     Call<ListWrapper<User>> getUserFromId(@Path("ids") StringBuilder userId);
 
-    @GET("2.2/users/{userId}/reputation-history?pagesize=30&site=stackoverflow")
+    @GET("2.2/users/{userId}/reputation-history?pageSize=30&site=stackOverflow")
     Call<ListWrapper<Reputation>> getReputationForUser(@Path("userId") String userId, @Query("page") int numberOfPage);
 
 }
