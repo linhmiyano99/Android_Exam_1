856b0f27aaffa789078086b08512e8161ff3ab2a
Linh <linhmiyano99>
2020-05-12 11:28:04 +0700

    update: code java coding convention

--- app/src/main/java/com/example/fossilandroidexam/data/model/stackoverflowservice/ListWrapper.java
+++ app/src/main/java/com/example/fossilandroidexam/data/model/stackoverflowservice/ListWrapper.java
@@ -2,7 +2,6 @@ package com.example.fossilandroidexam.data.model.stackoverflowservice;
 
 import java.util.List;
 
-// Because callback<T>
 public class ListWrapper<T> {
     public List<T> items;
 }
